afbefeac3ac4977db501b6d2dc56f5d1dc635b2d
Anand-Chawan <51610419+Anand-Chawan@users.noreply.github.com>
2020-08-14 19:41:49 +0530

    Add files via upload

---
+++ counting_triangles.cpp
@@ -0,0 +1,17 @@
+/*
+LINK: https://www.hackerearth.com/problem/algorithm/counting-triangles-4/description/
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define T ll t;cin>>t; for(ll ti=0;ti<t;ti++)
+ 
+int main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	T{
+		ll n;
+		cin>>n;
+		cout<<(8*n+6*(n-1))<<"\n";
+	}
+}
